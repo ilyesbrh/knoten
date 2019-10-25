a47da71404100fb303176ee205e6d25c9a5eb5b8
Safaa <37855620+arieme@users.noreply.github.com>
2019-10-26 00:51:19 +0100

    create project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MatIconRegistry } from '@angular/material';
+import { MatIconRegistry, MatDialog } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import { CreateProjectComponent } from '../create-project/create-project.component';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +12,22 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
+    private domSanitizer: DomSanitizer,
+    public dialog: MatDialog
   ) {
     this.matIconRegistry.addSvgIcon(
       'notification',
       this.domSanitizer.bypassSecurityTrustResourceUrl('../../assets/notification.svg')
     );
   }
+
+  createProject(): void {
+    const dialogRef = this.dialog.open(CreateProjectComponent, {
+      width: '748px',
+    });
+
+  }
+
   ngOnInit() {
   }
 
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -19,12 +19,7 @@ export class LandingComponent implements OnInit {
     });
 
   }
-  createProject(): void {
-    const dialogRef = this.dialog.open(CreateProjectComponent, {
-      width: '748px',
-    });
-
-  }
+  
 
  
   ngOnInit() {
