34ee22c00ad0e9f360aa595f72e924b5b164a28b
ilyesbrh <brhilyes@gmail.com>
2019-10-25 19:33:21 +0100

    empty home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,16 @@ import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 /* angular material components */
-import { MatButtonModule , MatCheckboxModule  } from '@angular/material';
+import { MatButtonModule , MatCheckboxModule,MatIconModule} from '@angular/material';
 import {MatDialogModule} from '@angular/material/dialog';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LandingComponent
+    LandingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +27,7 @@ import {MatDialogModule} from '@angular/material/dialog';
     /* material imports */
     MatButtonModule,
     MatDialogModule,
+    MatIconModule,
     MatCheckboxModule
 
   ],
